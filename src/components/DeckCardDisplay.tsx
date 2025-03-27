5167f75bd46c480c7b9f78ded77cdfeb7ba3ca82
thibault-kine <thibault.kine@laplateforme.io>
2025-03-27 02:12:04 +0100

    tofix: menuitem toujours clickable comme des boutons

--- src/components/DeckCardDisplay.tsx
+++ src/components/DeckCardDisplay.tsx
@@ -1,21 +1,70 @@
-import { Badge, Box } from "@mui/joy";
-import React from "react";
+import { AutoAwesome, SwapHoriz } from "@mui/icons-material";
+import { Badge, Box, Dropdown, Input, Menu, MenuButton, MenuItem } from "@mui/joy";
+import React, { useRef, useState } from "react";
 import { Card } from "scryfall-api";
 
 
 export default function DeckCardDisplay({ card, quantity }: { card: Card; quantity: number }) {
 
+    const [open, setOpen] = useState(false);
+    const buttonRef = useRef(null);
+
+
     return (
-        <>
-            <Box className="card-display">
-                <Badge badgeContent={quantity}>
-                    <img
-                        src={card.image_uris?.png || card.card_faces?.[0].image_uris?.png}
-                        alt={`${card.name} (${card.set})`}
-                        width={250}
+        <Dropdown open={open} onOpenChange={(e, isOpen) => setOpen(isOpen)}>
+            <MenuButton sx={{ display: 'contents' }} ref={buttonRef}>
+                <Box className="card-display">
+                    <Badge badgeContent={quantity}>
+                        <img
+                            src={card.image_uris?.png || card.card_faces?.[0].image_uris?.png}
+                            alt={`${card.name} (${card.set})`}
+                            width={250}
+                        />
+                    </Badge>
+                </Box>
+            </MenuButton>
+
+            <Menu placement="top-end" anchorEl={this}>
+                <MenuItem
+                    className="card-menu"
+                    onClick={() => window.open(card.scryfall_uri, '_blank', 'noreferrer')}
+                >
+                    See card on Scryfall <img src="/icons/other/scryfall.svg" width={25}/>
+                </MenuItem>
+                <MenuItem className="card-menu">Switch printing <AutoAwesome/></MenuItem>
+                <Box 
+                    sx={{ 
+                        display:"flex", 
+                        flexDirection: "row", 
+                        justifyContent: "space-between",
+                        padding: "5px",
+                        verticalAlign: "text-bottom",
+                        height: "fit-content",
+                        outline: "1px solid red",
+                        lineHeight: "30px"
+                    }}
+                    className="card-menu"
+                    onClick={(e) => e.preventDefault()}
+                >
+                    Change quantity
+                    <Input
+                        sx={{
+                            width: 'fit-content'
+                        }}
+                        onClick={(e) => e.stopPropagation()}
+                        defaultValue={1}
+                        type="number"
+                        slotProps={{
+                            input: {
+                                min: 1,
+                                max: 99,
+                                step: 1
+                            }
+                        }}
                     />
-                </Badge>
-            </Box>
-        </>
+                </Box>
+                <MenuItem className="card-menu">Move to sideboard <SwapHoriz/></MenuItem>
+            </Menu>
+        </Dropdown>
     )
 }
--- src/screens/DeckBuilder.tsx
+++ src/screens/DeckBuilder.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Box, Button, Dropdown, IconButton, Input, List, ListItem, Menu, MenuButton, MenuItem, Tab, TabList, TabPanel, Tabs, Textarea, Typography } from "@mui/joy";
-import { useUser } from "../context/UserContext";
+import React, { useState } from "react";
+import { Box, Button, Tab, TabList, TabPanel, Tabs, Textarea, Typography } from "@mui/joy";
 import { useNavigate } from "react-router-dom";
 import { saveDeckToUser } from "../../db/decks";
 import "../style/DeckBuilder.css"
 import BasicModal from "../components/BasicModal";
-import { AutoAwesome, MoreVert, OpenInNew, SwapHoriz } from "@mui/icons-material";
+import { MoreVert } from "@mui/icons-material";
 import ExportDeck from "../components/ExportDeck";
-import { getAllCards, searchCard } from "../../api/cards";
 import { Card, Cards } from "scryfall-api";
 import DeckCardDisplay from "../components/DeckCardDisplay";
 
@@ -17,8 +15,6 @@ export default function DeckBuilder({ user }) {
 
     const [deckList, setDeckList] = useState("");
     const [parsedDeck, setParsedDeck] = useState<{ card: Card; qty: number; }[]>([]);
-
-    const inputRef = useRef<HTMLInputElement>(null);
 
     const navigate = useNavigate();
 
@@ -114,51 +110,15 @@ export default function DeckBuilder({ user }) {
             </BasicModal>
 
             {parsedDeck.length > 0 && (
-                <Box>
-                    <Typography>Deck Preview:</Typography>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexWrap: "wrap",
-                        }}
-                    >
-                        {parsedDeck.map((entry, index) => (
-                            <Dropdown>
-                                <MenuButton sx={{ display: 'contents' }} key={index}>
-                                    <DeckCardDisplay card={entry.card} quantity={entry.qty} />
-                                </MenuButton>
-
-                                <Menu>
-                                    <MenuItem
-                                        className="card-menu"
-                                        onClick={() => window.open(entry.card.scryfall_uri, '_blank', 'noreferrer')}
-                                    >
-                                        See card on Scryfall <img src="/icons/other/scryfall.svg" width={25}/>
-                                    </MenuItem>
-                                    <MenuItem className="card-menu">Switch printing <AutoAwesome/></MenuItem>
-                                    <MenuItem 
-                                        className="card-menu"
-                                        onClick={(e) => e.preventDefault()}
-                                    >
-                                        Change quantity
-                                        <Input
-                                            defaultValue={1}
-                                            type="number"
-                                            slotProps={{
-                                                input: {
-                                                    ref: inputRef,
-                                                    min: 1,
-                                                    max: 99,
-                                                    step: 1
-                                                }
-                                            }}
-                                        />
-                                    </MenuItem>
-                                    <MenuItem className="card-menu">Move to sideboard <SwapHoriz/></MenuItem>
-                                </Menu>
-                            </Dropdown>
-                        ))}
-                    </Box>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                    }}
+                >
+                    {parsedDeck.map((entry, index) => (
+                        <DeckCardDisplay key={index} card={entry.card} quantity={entry.qty} />
+                    ))}
                 </Box>
             )}
         </Box>
