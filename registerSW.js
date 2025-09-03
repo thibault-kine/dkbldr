ee1aaca145dd4082b39f8a046b7a1bec65875c39
Thibault Kine <thibault.kine@laplateforme.io>
2025-09-03 09:27:35 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dkbldr/sw.js', { scope: '/dkbldr/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dkbldg/sw.js', { scope: '/dkbldg/' })})}
