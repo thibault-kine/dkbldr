ca998e922d38d9ebb1a9ea836359192cb1efc624
Thibault Kine <thibault.kine@laplateforme.io>
2026-07-08 14:28:23 +0200

    Added User-Agent

--- api/controllers/cardsController.js
+++ api/controllers/cardsController.js
@@ -1,6 +1,11 @@
+const headers = {
+    "User-Agent": "Dkbldr/1.0",
+    "Accept": "application/json"
+};
+
 async function getRandomCard(req, res) {
     try {
-        const response = await fetch("https://api.scryfall.com/cards/random");
+        const response = await fetch("https://api.scryfall.com/cards/random", { headers });
 
         console.log("Status: ", response.status);
         const body = await response.text();
@@ -29,7 +34,7 @@ async function getRandomCommander(req, res) {
     try {
         const query = encodeURIComponent("legal:edh is:commander -t:background");
 
-        const response = await fetch(`https://api.scryfall.com/cards/random?q=${query}`);
+        const response = await fetch(`https://api.scryfall.com/cards/random?q=${query}`, { headers });
 
         if (!response.ok) {
             return res.status(response.status).json({
@@ -54,7 +59,7 @@ async function getRandomCommander(req, res) {
 async function searchCard(req, res) {
     try {
         const query = encodeURIComponent(req.params.q);
-        const response = await fetch(`https://api.scryfall.com/cards/random?q=${query}`);
+        const response = await fetch(`https://api.scryfall.com/cards/random?q=${query}`, { headers });
 
         if (!response.ok) {
             return res.status(response.status).json({
@@ -75,7 +80,7 @@ async function searchCard(req, res) {
 async function getCardById(req, res) {
     try {
         const cardId = req.params.cardId;
-        const response = await fetch(`https://api.scryfall.com/cards/${cardId}`);
+        const response = await fetch(`https://api.scryfall.com/cards/${cardId}`, { headers });
 
         const card = await response.json();
 
@@ -90,7 +95,7 @@ async function getCardById(req, res) {
 async function getAllPrints(req, res) {
     try {
         const _card = await getCardById(req, res);
-        const result = await fetch(_card.prints_search_uri);
+        const result = await fetch(_card.prints_search_uri, { headers });
         
         const card = await result.json();
         return res.status(200).json(card);
